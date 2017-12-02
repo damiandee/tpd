bd7ed486ff2b4a6767ed2a3884a8191dfc427278
Dominika Loska <dloska@o2.pl>
2017-12-02 21:15:31 +0100

    Update Main with B Critical Path changes

--- Zadanie3/src/Main.java
+++ Zadanie3/src/Main.java
@@ -15,22 +15,31 @@ public class Main {
         pert.computeActionFactor(actionsList);
 
         Graph graph = new Graph(8, 11);
-        CriticalPath criticalPath = graph.getCriticalPath();
+        CriticalPath criticalPath = graph.getCriticalPathA();
+        CriticalPath criticalPathB = graph.getCriticalPathB();
 
         List<Action> criticalPathActionsList = getCriticalPathActions(criticalPath.getNodesNumbers());
+        List<Action> criticalPathActionsListB = getCriticalPathActions(criticalPathB.getNodesNumbers());
 
         double s_squareSum = getS_squareSum(criticalPathActionsList);
         double s = Math.sqrt(s_squareSum);
 
+        double s_squareSumB = getS_squareSum(criticalPathActionsListB);
+        double sB = Math.sqrt(s_squareSumB);
+
         double z = ((EXPECTED_TIME - criticalPath.getLength()) / s);
-        System.out.println(s_squareSum + ", " + s + ", " + z);
+        double zB = ((EXPECTED_TIME - criticalPathB.getLength()) / s);
+
+        System.out.println("Critical path A: " + s_squareSum + ", " + s + ", " + z);
+        System.out.println("Critical path B: " + s_squareSumB + ", " + sB + ", " + zB);
+
     }
 
     private static List<Action> getCriticalPathActions(List<Integer> nodesNumbers) {
         List<Action> criticalPathActionsList = new ArrayList<>();
-        for(int i = 0; i < nodesNumbers.size(); i++) {
-            for(Action action : actionsList) {
-                if(action.getI() == nodesNumbers.get(i) && action.getJ() == nodesNumbers.get(i + 1)) {
+        for (int i = 0; i < nodesNumbers.size(); i++) {
+            for (Action action : actionsList) {
+                if (action.getI() == nodesNumbers.get(i) && action.getJ() == nodesNumbers.get(i + 1)) {
                     criticalPathActionsList.add(action);
                 }
             }
@@ -40,7 +49,7 @@ public class Main {
 
     private static double getS_squareSum(List<Action> actionsList) {
         double sum = 0;
-        for(Action action : actionsList) {
+        for (Action action : actionsList) {
             sum += action.getS_square();
         }
         return sum;
