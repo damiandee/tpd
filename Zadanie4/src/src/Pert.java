4a4c64b53e248023445b46ae3b92669bbbeca7cf
Dominika Loska <dloska@o2.pl>
2018-01-05 20:30:38 +0100

    Add files via upload

--- Zadanie4/src/src/Pert.java
+++ Zadanie4/src/src/Pert.java
@@ -1,25 +1,45 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created by Damian Deska on 2017-12-02.
+ */
 public class Pert {
 
     private static int EXPECTED_TIME = 48;
 
     public void computeActionFactor(List<Action> actionsList) {
         for(Action action : actionsList) {
-            action.setT(computeT(action));
+           // action.setT(computeT(action));
+            //action.setS_square(computeS_square(action));
         }
     }
 
-    private double computeT(Action action) {
-        return ((action.getS()));
-    }
+//    private double computeT(Action action) {
+//        return ((action.getA() + (4.0* action.getM()) + action.getB()) / 6.0);
+//    }
+
+//    private double computeS_square(Action action) {
+//        double subtract = action.getB() - action.getA();
+//        double divide = subtract / 6.0;
+//        double s_square = Math.round(Math.pow(divide, 2) * 100);
+//        s_square = s_square/100;
+//        return s_square;
+//    }
 
     public void computeProbability(List<Action> actionsList) {
         Graph graph = new Graph(9, 15);
         CriticalPath criticalPath = graph.getCriticalPath(actionsList);
 
         List<Action> criticalPathActionsList = getCriticalPathActions(criticalPath.getNodesNumbers(), actionsList);
+
+        double s_squareSum = getS_squareSum(criticalPathActionsList);
+        double s = Math.sqrt(s_squareSum);
+
+        double z = ((EXPECTED_TIME - criticalPath.getLength()) / s);
+
+//        System.out.println("s_square: " + s_squareSum + "\ns: " + s + "\nt: " + criticalPath.getLength() +
+//                "\nz: " + z + "\n============================================");
     }
 
     private static List<Action> getCriticalPathActions(List<Integer> nodesNumbers, List<Action> actionsList) {
@@ -33,4 +53,13 @@ public class Pert {
         }
         return criticalPathActionsList;
     }
+
+    private static double getS_squareSum(List<Action> actionsList) {
+        double sum = 0;
+        for (Action action : actionsList) {
+            //sum += action.getS_square();
+        }
+        return sum;
+    }
+
 }
